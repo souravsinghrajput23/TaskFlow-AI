2be545db1744d4595e2e6df568a3ef82e266d674
Sourav Singh <swatirahulsingh1978@gmail.com>
2026-06-26 11:09:23 +0530

    fix api build

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,7 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
+import type { IncomingMessage, ServerResponse } from "http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -10,14 +11,15 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: IncomingMessage & { id?: string }) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+
+      res(res: ServerResponse) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,6 +27,7 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
